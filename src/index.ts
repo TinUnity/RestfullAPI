9c7836d99885d8c98ec0a2e682c3e85959822b92
TinHo <Honguyenthanhtin17@gmail.com>
2023-05-26 10:54:00 +0700

    no message

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
 import express from 'express';
 import cors from 'cors';
+import { createConnection } from 'typeorm';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 const port = Number(3000);
 
+// createConnection().then(async connection => {
+//     console.log('TypeOrm With Mongodb');
+    
+// }).catch(err => {
+//     console.log(err);
+// });
 app.use(require('./Routers/index'));
 
-app.listen(port,()=>console.log("connected to port:"+port));
+app.listen(port, () => console.log("connected to port:" + port));
