daab6442dd9346a269c027401cd7100dbd3f8e97
Tinho <honguyenthanhtin17@gmail.com>
2023-10-15 09:44:06 +0700

    add index

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,13 @@ import { User } from './Entities/UserDB';
 import { PlayerManager } from './Entities/PlayerManagerDB';
 import { Player } from './Entities/PlayerDB';
 import WebSocket from 'websocket';
+import { createServer } from 'http';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 const port = Number(3000);
+const server = createServer(app);
 
 createConnection({
     type: "mongodb",
@@ -27,29 +29,27 @@ createConnection({
     console.log('TypeOrm With Mongodb');
     app.use(require('./Routers/index'));
 
-    app.listen(port, () => console.log("connected to port:" + port));
+    console.log('Websocket is connected');
+    const webSocket = new WebSocket.server({ httpServer: server });
+    webSocket.on('request', (request) => {
+        const connection = request.accept(null, request.origin);
+
+        connection.on('message', (message) => {
+            if (message.type === 'utf8') {
+                console.log(`Received: ${message.utf8Data}`);
+
+                // Send a response back to the client
+                console.log("message: " + message);
+                connection.sendUTF(`You sent: ${message.utf8Data}`);
+            }
+        });
+
+        connection.on('close', (reasonCode, description) => {
+            console.log(`Client disconnected: ${reasonCode} - ${description}`);
+        });
+    });
+
+    server.listen(port, () => console.log("connected to port:" + port));
 }).catch(err => {
     console.log(err);
-});
-
-const webSocket = new WebSocket.server({
-    httpServer: app,
-});
-
-webSocket.on('request', (request) => {
-    const connection = request.accept(null,request.origin);
-
-    connection.on('message', (message) => {
-        if (message.type === 'utf8') {
-            console.log(`Received: ${message.utf8Data}`);
-
-            // Send a response back to the client
-            console.log("message: "+message);
-            connection.sendUTF(`You sent: ${message.utf8Data}`);
-        }
-    });
-
-    connection.on('close', (reasonCode, description) => {
-        console.log(`Client disconnected: ${reasonCode} - ${description}`);
-    });
 });
