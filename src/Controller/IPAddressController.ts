07498e239515760f735d2b3f4c580b14412804b5
Tinhnt <tinhnt@bap.jp>
2024-06-29 16:34:50 +0700

    - Update ipaddress

--- src/Controller/IPAddressController.ts
+++ src/Controller/IPAddressController.ts
@@ -3,9 +3,9 @@ import bodyParser from 'body-parser';
 
 var controller = express();
 controller.use(bodyParser.json());
-controller.get('/getIP', async (res,req) => {
-    const ipAddress = req.socket.remoteAddress;
-    console.log(ipAddress);
+controller.get('/ipv4', function (req,res) {
+    console.log("Your IP Addresss is: " + req.socket.remoteAddress);
+    res.send("Your IP Addresss is: " + req.socket.remoteAddress);
 });
 
 module.exports = controller;
