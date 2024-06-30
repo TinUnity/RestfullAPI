926dbb300c4cb8155c1c408bffb36efd363cb429
Tinhnt <tinhnt@bap.jp>
2024-06-30 15:01:42 +0700

    no message

--- src/Controller/IPAddressController.ts
+++ src/Controller/IPAddressController.ts
@@ -1,11 +1,11 @@
-import express from 'express';
-import bodyParser from 'body-parser';
+// import express from 'express';
+// import bodyParser from 'body-parser';
 
-var controller = express();
-controller.use(bodyParser.json());
-controller.get('/ipv4', function (req,res) {
-    console.log("Your IP Addresss is: " + req.socket.remoteAddress);
-    res.send("Your IP Addresss is: " + req.socket.remoteAddress);
-});
+// var controller = express();
+// controller.use(bodyParser.json());
+// controller.get('/ipv4', function (req,res) {
+//     console.log("Your IP Addresss is: " + req.socket.remoteAddress);
+//     res.send("Your IP Addresss is: " + req.socket.remoteAddress);
+// });
 
-module.exports = controller;
+// module.exports = controller;
