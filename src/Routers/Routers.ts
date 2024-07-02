0da220ade25e58b5e2f93c3295eb1b02dc03c234
Tinhnt <tinhnt@bap.jp>
2024-07-02 22:00:21 +0700

    - Update user db

--- src/Routers/Routers.ts
+++ src/Routers/Routers.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 
 const routes = express.Router();
-// routes.use('/user',require('../Controller/UserController'));
+routes.use('/user',require('../Controller/UserController'));
 // routes.use('/mail',require('../Controller/MailController').controller);
 // routes.use('/player',require('../Controller/PlayerController'))
 // routes.use('/ip',require('../Controller/IPAddressController'))
